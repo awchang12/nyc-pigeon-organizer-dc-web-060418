8cae1e61ec7755a8d307b62e9f225e7c1313c546
Anthony Chang <awchang12@gmail.com>
2018-05-15 22:43:09 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,5 +5,6 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value_hash|
     value_hash.each do |attribute, birds|
       birds.each do |bird|
-        if organized_pigeons[bird]
+        if !organized_pigeons[bird]
+          
 end
