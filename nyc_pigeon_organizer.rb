2fdfad55ff18c8fca5289900fcb44b3fcbd08398
Anthony Chang <awchang12@gmail.com>
2018-05-15 22:44:12 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,5 +7,5 @@ def nyc_pigeon_organizer(data)
       birds.each do |bird|
         if !organized_pigeons[bird]
           organized_pigeons[bird] = {}
-          organized_pigeons[bird][key] = []
+          organized_pigeons[bird][key] = [] << attribute.to_s
 end
