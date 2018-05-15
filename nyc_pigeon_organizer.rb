53df1be6a08de3b56b0fef6358a21b9983d3af4b
Anthony Chang <awchang12@gmail.com>
2018-05-15 22:48:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_pigeons = {}
+  
+  data.each do |key, value_hash|
+    value_hash.each do |attribute, birds|
+      birds.each do |bird|
+        if !organized_pigeons[bird]
+          organized_pigeons[bird] = {}
+          organized_pigeons[bird][key] = [] << attribute.to_s
+        elsif !organized_pigeons[bird][key]
+          organized_pigeons[bird][key] = [] << attribute.to_s
+        else
+          organized_pigeons[bird][key] << attribute.to_s
+        end
+      end
+    end
+  end
+  organized_pigeons
 end
